1477d2cd458fc7a81e9df83af6a4b60f2861ae8f
ElijahGM <dev@barcode-kenya.com>
2017-06-19 18:20:22 +0300

    Default codebase

---
+++ src/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+namespace tooma\api;
+use Yii;
+use yii\authclient\Collection;
+use yii\base\BootstrapInterface;
+use yii\console\Application as ConsoleApplication;
+
+class Bootstrap implements BootstrapInterface{
+	
+
+}
---
+++ src/Sms.php
@@ -0,0 +1,26 @@
+<?php
+namespace tooma\api;
+use Yii;
+use yii\base\Component;
+use tooma/sms-api/
+class Sms extends Component{
+	public $apiKey;
+	public $sslPath=null;
+	private $sender;
+
+
+    /** @inheritdoc */
+    public function init()
+    {
+        $this->sender = new Tooma($this->apiKey,$this->sslPath);;
+        parent::init();
+    }
+	public function _call($method,$args)
+	{
+		if(method_exists($this->sender, $method)){
+			return call_user_method_array($method, $this->sender, $args);
+		}
+		throw new ToomaMethodException("No method '$method' defined in our interface", 5689);
+		
+	}
+}
